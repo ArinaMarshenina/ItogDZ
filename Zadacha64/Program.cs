f019529c2004fc17cc0f7255e55262b51ef05ea8
Arina M <apelsinka7.ru@yandex.ru>
2022-10-18 23:23:43 +0300

    решение задачи 64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,18 @@
+﻿int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void WriteNumbersRecursionVoid(int startNumber, int lastNumber)
+{
+    if (lastNumber <= startNumber)
+    {
+        Console.WriteLine(startNumber );
+        WriteNumbersRecursionVoid(startNumber-1, lastNumber);
+    }     
+}
+
+int n = GetNumber();
+Console.WriteLine();
+WriteNumbersRecursionVoid(n, 1);
